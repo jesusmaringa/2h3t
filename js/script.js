dae2af8c130a6c55bc080458a6777d47805a1233
Jesus <101044903+jesusmaringa@users.noreply.github.com>
2023-11-24 07:41:12 -0300

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 let num1 = "";
 let num2 = "";
 let valor = "";
-let temPonto = false; 
+let temPonto = false;
 let executa = "";
 mensagem = (msg) => console.log(msg);
 soma = (a,b) => a + b;
@@ -27,25 +27,58 @@ function digitando(tecla){
    valor += tecla;
    mostrar_display(valor);
 }
-function operacao(op){
-    executa = op;
-    num1 = valor;
-    valor = "";
+function operacao (op){
+  executa = op;
+  num1 = valor;
+  valor = "";
 }
 
 function calcula(){
-   if(executa != ""){
-     num2 = valor;
-     if(executa == "soma") mostrar_display(soma(Number(num1),Number(num2)));
-     if(executa == "sub") mostrar_display(sub(num1,num2));
-     if(executa == "div") mostrar_display(div(num1,num2));
-     if(executa == "mult") mostrar_display(mult(num1,num2));
-     num1 = "";
-     num2 = "";
-     valor = "";
-     temPonto = false;
-     executa = "";
-   }
+  if(executa != ""){
+    num2 = valor;
+    if(executa == "soma") mostrar_display(soma(Number(num1),Number(num2)));
+    if(executa == "sub") mostrar_display(sub(num1,num2));
+    if(executa == "div") mostrar_display(div(num1,num2));
+    if(executa == "mult") mostrar_display(mult(num1,num2));
+    num1 = "";
+    num2 = "";
+    valor = "";
+    temPonto = false;
+    executa = "";
+  }
 }
 
+let a = 0;
+let b = 0;
+let c = 0;
+function calcula_eq(){
+  if(a != 0){
+    let delta = (b*b) - (4*a*c);
+    document.getElementById("delta").innerHTML = delta;
+  }
+}
+function muda_sinal_a(){
+  a = (-1)*a;
+  calcula_eq();
+}
+function muda_sinal_b(){
+  b = (-1)*b;
+  calcula_eq();
+}
+function muda_sinal_c(){
+  c = (-1)*c;
+  calcula_eq();
+}
+function set_valor_a(){
+  a = document.getElementById("valor_a").value;
+  calcula_eq();
+}
+function set_valor_b(){
+  b = document.getElementById("valor_b").value;
+  calcula_eq();
+}
+function set_valor_c(){
+  c = document.getElementById("valor_c").value;
+  calcula_eq();
+}
 
